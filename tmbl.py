ef4978e4f099f2939ff5723d7be0619e6eb76f57
cct90308 <139643752+cct90308@users.noreply.github.com>
2023-07-16 11:38:14 +0800

    Add files via upload
    Signed-off-by: cct90308 <139643752+cct90308@users.noreply.github.com>

--- tmbl.py
+++ tmbl.py
@@ -81,6 +81,8 @@ def select_record_type():
 
 def calculate_rank_all_players(data, statistics):
     return data[statistics].rank(pct=True)
+
+
     
 # Streamlit app
 def main():
@@ -110,8 +112,11 @@ def main():
     records = 'all'
     types = [0, 1, 2]
     
+    
     # Data dictionary to store different data
     data_dict = {}
+    
+    
     
     # Check if CSV file exists
     filename = f"{startyear}_{endyear}_{stat}_{games}_{playerstatus}.csv"
@@ -129,8 +134,6 @@ def main():
                 type_value = types[i % len(types)]
                 
                 time.sleep(3)
-                # Pause for 3 seconds
-                
                 # Generate the URL with selected variable values
                 http = f"https://atl-01.statsplus.net/tmbl/playerstats/?sort=ops,d&stat={stat}&team={team}&qual={qual}&pos={pos}&more=true&games={games}&startyear={startyear}&endyear={endyear}&rightleft={rightleft}&playerstatus={playerstatus}&timespan={timespan}&startdate={startdate}&enddate={enddate}&records={records}&type={type_value}"
 
@@ -140,7 +143,7 @@ def main():
                 # 解析网页内容并提取球员统计数据
                 table = soup.find('table')
                 data = pd.read_html(str(table))[0]
-                st.text(f"Fetching data... Progress: {i+1}/3")
+                st.text(f"Fetching data... Progress: {i + 1}/3")
          
                 # Store the data in the data dictionary
                 data_dict[f"data{i+1}"] = data
@@ -160,97 +163,101 @@ def main():
             data.to_csv(filename, index=False)
             data_dict['data'] = data
     
-    # Display the final data
-    #st.dataframe(data)
-
-    # 获取所有球员名称列表
-    player_names = data['Name'].unique().tolist()
-    # 选择要显示的球员
-    selected_players = st.multiselect('Select a player', player_names)
-    #selected_players = []
-    pos = ""
+            # Display the final data
+            #st.dataframe(data)
+        else:
+            st.text("輸入年份並按下RUN")
     
-    run2_button = st.button("Plot")
-    if run2_button:
-        if selected_players:
-            pos = data[data['Name'] == selected_players[0]]['Pos'].iloc[0]
+    if "data" in data_dict:
+        data = data_dict["data"]
 
-            # 设置图表样式
-            plt.style.use('seaborn')
+        # 获取所有球员名称列表
+        player_names = data['Name'].unique().tolist()
+        # 选择要显示的球员
+        selected_players = st.multiselect('Select a player', player_names)
+        pos = ""
+    
+        run2_button = st.button("Plot")
+        if run2_button:
+            if selected_players:
+                pos = data[data['Name'] == selected_players[0]]['Pos'].iloc[0]
 
-            # 图表尺寸
-            fig, axes = plt.subplots(nrows=4, ncols=3, figsize=(9, 6))
+                # 设置图表样式
+                plt.style.use('seaborn')
 
-            # 设置背景为透明
-            for ax in axes.flatten():
-                ax.patch.set_alpha(0)
-                ax.set_xticks([])
-                ax.set_yticks([])
+                # 图表尺寸
+                fig, axes = plt.subplots(nrows=4, ncols=3, figsize=(9, 6))
 
-            # 创建渐进色映射
-            cmap = mcolors.LinearSegmentedColormap.from_list('custom_cmap', [(0, 'blue'), (1, 'red')])
+                # 设置背景为透明
+                for ax in axes.flatten():
+                    ax.patch.set_alpha(0)
+                    ax.set_xticks([])
+                    ax.set_yticks([])
 
-            # 搜寻指标
-            statistics = ['WRC+', 'WOBA', 'BB%', 'K%', 'OPS', 'BABIP', 'HR']
+                # 创建渐进色映射
+                cmap = mcolors.LinearSegmentedColormap.from_list('custom_cmap', [(0, 'blue'), (1, 'red')])
 
-            # 使用@st.cache_data註釋的函數將被緩存，避免重複計算
-            rank_all_players = calculate_rank_all_players(data, statistics)
+                # 搜寻指标
+                statistics = ['WRC+', 'WOBA', 'BB%', 'K%', 'OPS', 'BABIP', 'HR']
 
-            for i, statistic in enumerate(statistics):
-                for j, player_name in enumerate(selected_players):
-                    # 选择特定球员的数据
-                    player_data = data[data['Name'] == player_name]
+                # 使用@st.cache_data註釋的函數將被緩存，避免重複計算
+                rank_all_players = calculate_rank_all_players(data, statistics)
 
-                    # 计算特定球员在其他指标上的排名百分比
-                    rank_data = rank_all_players.loc[player_data.index, statistic].iloc[0]
-                    rank_data = round(rank_data, 2)
-                    # 计算行和列索引
-                    row = i // 3
-                    col = i % 3
+                for i, statistic in enumerate(statistics):
+                    for j, player_name in enumerate(selected_players):
+                        # 选择特定球员的数据
+                        player_data = data[data['Name'] == player_name]
 
-                    # 创建子图
-                    ax = axes[row, col]
+                        # 计算特定球员在其他指标上的排名百分比
+                        rank_data = rank_all_players.loc[player_data.index, statistic].iloc[0]
+                        rank_data = round(rank_data, 2)
+                        # 计算行和列索引
+                        row = i // 3
+                        col = i % 3
 
-                    # 图表设置
-                    height = 0.25
-                    ax.barh(0, 1, 1, color='gray', alpha=0)
-                    bar = patches.Rectangle((0, -height / 6), 1, height/3, color='gray')
-                    ax.add_patch(bar)
+                        # 创建子图
+                        ax = axes[row, col]
 
-                    circle_step_radius = height * 0.22
-                    circle_step1 = Circle((0, 0), radius=circle_step_radius, color='gray')
-                    ax.add_patch(circle_step1)
-                    circle_step2 = Circle((0.5, 0), radius=circle_step_radius, color='gray')
-                    ax.add_patch(circle_step2)
-                    circle_step3 = Circle((1, 0), radius=circle_step_radius, color='gray')
-                    ax.add_patch(circle_step3)
+                        # 图表设置
+                        height = 0.25
+                        ax.barh(0, 1, 1, color='gray', alpha=0)
+                        bar = patches.Rectangle((0, -height / 6), 1, height/3, color='gray')
+                        ax.add_patch(bar)
 
-                    # 添加百分比文本在圆形标记内部
-                    circle_radius = height * 0.5
-                    circle_x = rank_data + circle_radius / 2  # 圆形标记的x坐标
-                    circle_y = 0  # 圆形标记的y坐标
-                    circle_color = cmap(rank_data)  # 使用渐进色映射确定圆形标记的颜色
-                    circle = Circle((circle_x, circle_y), radius=circle_radius, color=circle_color)
-                    ax.add_patch(circle)
-                    ax.text(circle_x, circle_y, f'{int((rank_data) * 100)}', ha='center', va='center', color='white', weight='bold')
+                        circle_step_radius = height * 0.22
+                        circle_step1 = Circle((0, 0), radius=circle_step_radius, color='gray')
+                        ax.add_patch(circle_step1)
+                        circle_step2 = Circle((0.5, 0), radius=circle_step_radius, color='gray')
+                        ax.add_patch(circle_step2)
+                        circle_step3 = Circle((1, 0), radius=circle_step_radius, color='gray')
+                        ax.add_patch(circle_step3)
 
-                    # 设置标题
-                    ax.set_title(f"{statistic}: {player_data[statistic].iloc[0]}")
+                        # 添加百分比文本在圆形标记内部
+                        circle_radius = height * 0.5
+                        circle_x = rank_data + circle_radius / 2  # 圆形标记的x坐标
+                        circle_y = 0  # 圆形标记的y坐标
+                        circle_color = cmap(rank_data)  # 使用渐进色映射确定圆形标记的颜色
+                        circle = Circle((circle_x, circle_y), radius=circle_radius, color=circle_color)
+                        ax.add_patch(circle)
+                        ax.text(circle_x, circle_y, f'{int((rank_data) * 100)}', ha='center', va='center', color='white', weight='bold')
 
-            fig.suptitle(', '.join(selected_players)+'  '+str(pos), fontsize=16, fontweight='bold')
-            fig.text(0.5, 0.92, '1995 Regular Season PR  (Min 25 PA)', fontsize=12, ha='center')
-            fig.text(0.5, 0.33, 'Ranked by Pos  (Min 25 IP)', fontsize=12, ha='center')
+                        # 设置标题
+                        ax.set_title(f"{statistic}: {player_data[statistic].iloc[0]}")
 
-            # 显示图表
-            st.pyplot(fig)
-        
-    # Delete CSV file when Streamlit is closed
-    #if not get_session_state()['filename_deleted']:
-        #st.text("Waiting for Streamlit to close...")
-        #if st.button("Delete CSV"):
-            #filename = f"{startyear}_{endyear}.csv"
-            #os.remove(filename)
-            #get_session_state()['filename_deleted'] = True
+                fig.suptitle(', '.join(selected_players)+'  '+str(pos), fontsize=16, fontweight='bold')
+                fig.text(0.5, 0.92, '1995 Regular Season PR  (Min 25 PA)', fontsize=12, ha='center')
+                fig.text(0.5, 0.33, 'Ranked by Pos  (Min 25 IP)', fontsize=12, ha='center')
+
+                # 显示图表
+                st.pyplot(fig)
+            
+        # Delete CSV file when Streamlit is closed
+        #if not get_session_state()['filename_deleted']:
+            #st.text("Waiting for Streamlit to close...")
+            #if st.button("Delete CSV"):
+                #filename = f"{startyear}_{endyear}.csv"
+                #os.remove(filename)
+                #get_session_state()['filename_deleted'] = True
         
         
 
